94a980a67f9d6292e1ee10e94e03ba92d444e103
Changyang Xu <xcycharles@users.noreply.github.com>
2021-05-15 11:49:29 +0800

    change

--- hw3 monte carlo.py
+++ hw3 monte carlo.py
@@ -3,6 +3,7 @@ import numpy as np
 import IPython
 import BSM_class
 import random
+import pandas as pd
 IPython.get_ipython().magic("matplotlib auto")  # show interactive plot not inline
 
 
@@ -21,9 +22,10 @@ class European_Call_Payoff:
 
 class UpandOutBarrier_European_Call_Payoff:
 
-    def __init__(self, strike, barrier_level1=0, barrier_level2=0, barrier_level3=0, barrier_level4=0, barrier_level5=0, barrier_level6=0):
+    def __init__(self, strike, barrier_level1=0, barrier_level2=0, barrier_level3=0, barrier_level4=0, barrier_level5=0, barrier_level6=0, rebate=0):
         # default values are set so all inputs are optional
         self.strike = strike
+        self.rebate = rebate
         self.barrier_level1 = barrier_level1
         self.barrier_level2 = barrier_level2
         self.barrier_level3 = barrier_level3
@@ -63,7 +65,7 @@ class UpandOutBarrier_European_Call_Payoff:
             else:
                 return 0
         else:
-            return 0
+            return self.rebate
 
 
 class GeometricBrownianMotion:
@@ -91,7 +93,7 @@ class GeometricBrownianMotion:
             self.T -= self.dt  # Account for the step in time
 
 
-def main():
+def main(rebate):
 
     # Generate a set of sample brownian motion paths
     for i in range(0, paths):
@@ -119,12 +121,12 @@ def main():
     for price_path in price_paths:
 
         # initiate here due to internal class flags need to be reset for each price_path
-        bc_case1 = UpandOutBarrier_European_Call_Payoff(strike_price, 110)  # 110 is the barrier price
-        bc_case2 = UpandOutBarrier_European_Call_Payoff(strike_price, 110, 108)
-        bc_case3 = UpandOutBarrier_European_Call_Payoff(strike_price, 105, 106, 107, 108, 109, 110)
-        bc_case41 = UpandOutBarrier_European_Call_Payoff(strike_price, 110)
-        bc_case42 = UpandOutBarrier_European_Call_Payoff(strike_price, 110)
-        bc_case43 = UpandOutBarrier_European_Call_Payoff(strike_price, 110)
+        bc_case1 = UpandOutBarrier_European_Call_Payoff(strike_price, 110, rebate=rebate)  # 110 is the barrier price
+        bc_case2 = UpandOutBarrier_European_Call_Payoff(strike_price, 110, 108, rebate=rebate)
+        bc_case3 = UpandOutBarrier_European_Call_Payoff(strike_price, 105, 106, 107, 108, 109, 110, rebate=rebate)
+        bc_case41 = UpandOutBarrier_European_Call_Payoff(strike_price, 110, rebate=rebate)
+        bc_case42 = UpandOutBarrier_European_Call_Payoff(strike_price, 110, rebate=rebate)
+        bc_case43 = UpandOutBarrier_European_Call_Payoff(strike_price, 110, rebate=rebate)
 
         # last three month check barrier
         for price in price_path[-90:]: # this is in chronological order
@@ -171,19 +173,19 @@ def main():
         case43_call_payoffs.append(bc_case43.get_payoff(price_path[-1]) / np.exp(risk_free_rate * T))
         vanilla_call_payoffs.append(ec.get_payoff(price_path[-1]) / np.exp(risk_free_rate*T))
 
-
-    print('European Vanilla Option simulation price is: ', np.average(vanilla_call_payoffs))
-    print('European Case1 Option simulation price is: ', np.average(case1_call_payoffs))
-    print('European Case2 Option simulation price is: ', np.average(case2_call_payoffs))
-    print('European Case3 Option simulation price is: ', np.average(case3_call_payoffs))
-    print('European Case41 Option simulation price is: ', np.average(case41_call_payoffs))
-    print('European Case42 Option simulation price is: ', np.average(case42_call_payoffs))
-    print('European Case43 Option simulation price is: ', np.average(case43_call_payoffs))
-
     trade = BSM_class.OptionTrade(initial_price, strike_price, risk_free_rate, volatility, T, dividend_yield, 1)
-    print('European Option BSM price is: ', trade.BSM())
+    # print('European Option BSM price is: ', trade.BSM())
+    # print('European Vanilla Option simulation price is: ', np.average(vanilla_call_payoffs))
+    # print('European Case1 Option simulation price is: ', np.average(case1_call_payoffs))
+    # print('European Case2 Option simulation price is: ', np.average(case2_call_payoffs))
+    # print('European Case3 Option simulation price is: ', np.average(case3_call_payoffs))
+    # print('European Case4_monthly Option simulation price is: ', np.average(case41_call_payoffs))
+    # print('European Case4_weekly Option simulation price is: ', np.average(case42_call_payoffs))
+    # print('European Case4_daily Option simulation price is: ', np.average(case43_call_payoffs))
     # BSM will match brownian motion monte carlo pricing if drift is zero!
     # no one is better than the other, they are just different approaches
+
+    return [trade.BSM(),np.average(vanilla_call_payoffs),np.average(case1_call_payoffs),np.average(case2_call_payoffs),np.average(case3_call_payoffs),np.average(case41_call_payoffs),np.average(case42_call_payoffs),np.average(case43_call_payoffs)]
 
 
 if __name__ == "__main__":
@@ -191,7 +193,7 @@ if __name__ == "__main__":
     # Model Parameters
 
     random.seed(1)
-    paths = 10000
+    paths = 1000
     initial_price = 100.0
     strike_price = 105.0
     volatility = 0.3
@@ -204,4 +206,8 @@ if __name__ == "__main__":
 
     # Main
 
-    main()
+    df = pd.DataFrame([],index=['BSM price','vanilla price','constant barrier price','step-up barrier price','linear barrier price','discrete barrier monthly','discrete barrier weekly','discrete barrier daily'])
+    for i in [0,4,8]:
+        df['rebate=$'+str(i)]=main(i)
+
+display(df)
